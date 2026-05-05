683aacca4bce3144c9ee47a2d094dbc845f47edd
Zeiad Mohammed <zeiadm4business@gmail.com>
2026-05-05 16:29:33 -0700

    Initial iOS project setup

---
+++ SportX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportX
+//
+//  Created by Zeiad Mohammed on 05/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
